aa4b7e2a17e11cca885bfc0bf29ea70b88a8cf6d
SeongmoAhn <asm0619@naver.com>
2023-07-11 16:05:37 +0900

    student

---
+++ Student.h
@@ -0,0 +1,22 @@
+// Student.h
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+    int id; // 학번
+    string name; // 이름
+    string dept; // 학과
+
+public:
+    Student(int id, string name, string dept) {
+        this->id = id;
+        this->name = name;
+        this->dept = dept;
+    }
+
+    void display() {
+        cout << "학번 : " << id << "이름 : " << name << "학과 : " << dept << endl;
+    }
+};
