3a7e508e05ce3f3804310432e7d6425c4000ccbe
SeongmoAhn <asm0619@naver.com>
2023-07-11 16:26:28 +0900

    연결 리스트를 이용한 스택

--- stack/linkedStack.cpp
+++ stack/linkedStack.cpp
@@ -7,6 +7,15 @@ int main() {
     stack.push(new Node(20230002, "이세상", "경제학과"));
     stack.push(new Node(20230003, "이윤성", "소프트웨어학과"));
     stack.display();
+    cout << endl;
+
+    Node *popNode = stack.pop();
+    cout << "[pop()된 노드]" << endl;
+    popNode->display();
+    delete popNode;
+    cout << endl;
+
+    stack.display();
 
     return 0;
 }
