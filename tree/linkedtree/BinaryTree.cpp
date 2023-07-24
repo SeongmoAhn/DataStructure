072e6b42221a63fa23e63f0acdd7feb85aef51f0
SeongmoAhn <asm0619@naver.com>
2023-07-24 12:41:48 +0900

    add tree traversal

--- tree/linkedtree/BinaryTree.cpp
+++ tree/linkedtree/BinaryTree.cpp
@@ -4,14 +4,17 @@
 int main() {
     BinaryTree tree;
 
+    BinaryNode *G = new BinaryNode(7, NULL, NULL);
     BinaryNode *F = new BinaryNode(6, NULL, NULL);
     BinaryNode *E = new BinaryNode(5, NULL, NULL);
     BinaryNode *D = new BinaryNode(4, NULL, NULL);
-    BinaryNode *C = new BinaryNode(3, F, NULL);
+    BinaryNode *C = new BinaryNode(3, F, G);
     BinaryNode *B = new BinaryNode(2, D, E);
     BinaryNode *A = new BinaryNode(1, B, C);
 
     tree.setRoot(A);
+
+    tree.display();
 
     return 0;
 }
--- tree/linkedtree/BinaryTree.h
+++ tree/linkedtree/BinaryTree.h
@@ -1,5 +1,6 @@
 // BinaryTree.h
 #include "BinaryNode.h"
+using namespace std;
 
 class BinaryTree {
     BinaryNode *root;
@@ -12,16 +13,50 @@ public:
 
     bool isEmpty() { return (root == NULL); }
 
-    // void preorder() {...}
-    // void inorder() {...}
-    // void postorder() {...}
-    // void levelorder() {...}
-    // void display() {
-    //     preorder();
-    //     inorder();
-    //     postorder();
-    //     levelorder();
+    void preorder() {
+        cout << "preorder  : ";
+        preorder(root);
+        cout << endl;
+    }
+    void preorder(BinaryNode *node) {
+        if (node == NULL) return ;
+
+        cout << "[" << node->getData() << "] ";
+        preorder(node->getLeft());
+        preorder(node->getRight());
+    }
+    void inorder() {
+        cout << "inorder   : ";
+        inorder(root);
+        cout << endl;
+    }
+    void inorder(BinaryNode *node) {
+        if (node == NULL) return;
+
+        inorder(node->getLeft());
+        cout << "[" << node->getData() << "] ";
+        inorder(node->getRight());
+    }
+    void postorder() {
+        cout << "postorder : ";
+        postorder(root);
+        cout << endl;
+    }
+    void postorder(BinaryNode *node) {
+        if (node == NULL) return;
+
+        postorder(node->getLeft());
+        postorder(node->getRight());
+        cout << "[" << node->getData() << "] ";
+    }
+    // void levelorder() {
     // }
+    void display() {
+        preorder();
+        inorder();
+        postorder();
+        // levelorder();
+    }
 
     // int getCount() {...}
     // int getHeight() {...}
