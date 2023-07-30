859701f3adaaa19e9637ecc737234e07caa7c55d
SeongmoAhn <asm0619@naver.com>
2023-07-30 15:38:54 +0900

    add heap sort

--- queue/priority/MaxHeap.cpp
+++ queue/priority/MaxHeap.cpp
@@ -1,36 +1,36 @@
 // MaxHeap.cpp
+#include <cstdlib>
+#include <time.h>
 #include "MaxHeap.h"
 
 int main() {
     MaxHeap heap;
+    int arr[10];
 
-    heap.insert(10); heap.insert(5);
-    heap.insert(30); heap.insert(8);
-    heap.insert(9); heap.insert(3);
-    heap.insert(7);
+    srand(time(NULL));
+    for (int i = 0; i < 10; i++) {
+        arr[i] = rand() % 100;
+    }
 
-    heap.display();
+    cout << "정렬 전" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "[" << arr[i] << "] ";
+    } cout << endl;
 
-    heap.remove(); heap.display();
-    heap.remove(); heap.display();
+    heap.heapSort(arr, 10);
+
+    cout << "정렬 후" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "[" << arr[i] << "] ";
+    } cout << endl;
 
     return 0;
 }
 
 /*
 실행 결과:
-Heap 내용
-[30]
-[9][10]
-[5][8][3][7]
-
-Heap 내용
-[10]
-[9][7]
-[5][8][3]
-
-Heap 내용
-[9]
-[8][7]
-[5][3]
+정렬 전
+[0] [87] [29] [94] [20] [47] [20] [50] [11] [2]
+정렬 후
+[0] [2] [11] [20] [20] [29] [47] [50] [87] [94]
 */
