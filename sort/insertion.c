340a078851ffda967bf87e9ad115bc911fc4498e
SeongmoAhn <asm0619@naver.com>
2023-08-30 11:16:03 +0900

    insertion sort

---
+++ sort/insertion.c
@@ -0,0 +1,27 @@
+// insertion.c
+#include "array.h"
+#define SIZE 10
+#define MOD 100
+
+void insertionSort(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int idx;
+        for (idx = i - 1; idx >= 0 && arr[idx] > key; idx--) {
+            arr[idx + 1] = arr[idx];
+        }
+        arr[idx + 1] = key;
+    }
+}
+
+int main() {
+    int arr[SIZE];
+
+    getRandomArray(arr, SIZE, MOD);
+    printArray(arr, SIZE);
+
+    insertionSort(arr, SIZE);
+    printArray(arr, SIZE);
+
+    return 0;
+}
