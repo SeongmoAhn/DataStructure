605ba4adba02b6ae3f382327b1e9db866a9ebc15
SeongmoAhn <asm0619@naver.com>
2023-08-30 09:44:59 +0900

    정렬 헤더파일

---
+++ sort/array.h
@@ -0,0 +1,25 @@
+// randomArray.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void getRandomArray(int arr[], int size, int mod) {
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % mod + 1;
+    }
+}
